1ebae0c756ed6a9b4bb993ede7a3ceb828aacfd0
markyangkp <markyangkp@outlook.com>
2023-11-25 21:12:44 +0800

    wda

--- gobang_frontend/js/gamePvP.js
+++ gobang_frontend/js/gamePvP.js
@@ -12,8 +12,39 @@ function initBoard() {
         }
     }
 }
-function isPlayChess(){
-    
+// 引入socket.io-client库
+// import io from 'socket.io-client';
+function testF() {
+    // 连接到后端的Socket.IO服务器
+    const socket = io('http://127.0.0.1:5000');
+
+    // 发送joinRoom事件
+    socket.emit('joinRoom', {
+        userID: 'yourUserID',  // 替换为实际的用户ID
+        roomID: 'yourRoomID'   // 替换为实际的房间ID
+    });
+
+    // 监听game_start事件
+    socket.on('game_start', (data) => {
+        console.log('游戏开始:', data);
+
+        // 在这里处理游戏开始的逻辑
+        // 例如，更新页面上的内容，显示游戏界面等
+    });
+
+    // 监听Error事件
+    socket.on('Error', (errorMessage) => {
+        console.error('发生错误:', errorMessage);
+
+        // 在这里处理错误的逻辑
+        // 例如，显示错误消息给用户等
+    });
+
+}
+
+
+function isPlayChess() {
+
 }
 var playerType = 0
 function handleCellClick(event) {
