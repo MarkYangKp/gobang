37b0e12a690477c8d46182b15ed84fc47589a896
markyangkp <markyangkp@outlook.com>
2023-11-26 00:57:36 +0800

    判断胜利

--- gobang_frontend/js/gamePvP.js
+++ gobang_frontend/js/gamePvP.js
@@ -16,15 +16,23 @@ for (var i = 0; i < 19; i++) {
     }
 }
 
-function initBoard() {
+//渲染棋盘
+function RenderBoard() {
     const boardElement = document.getElementById('boardPad');
     boardElement.innerHTML = '';
     for (var i = 0; i < 19; i++) {
         for (var j = 0; j < 19; j++) {
             const cell = document.createElement('div');
-            cell.className = 'cell-null';
             cell.dataset.row = i;
             cell.dataset.col = j;
+            if (boardData[i][j] == 0) {
+                cell.className = 'cell-null';
+            } else if (boardData[i][j] == 1) {
+                cell.className = 'cell-black';
+            } else if (boardData[i][j] == 2) {
+                cell.className = 'cell-white';
+            }
+
             cell.addEventListener('click', chooseBlock);
             boardElement.appendChild(cell);
         }
@@ -72,10 +80,10 @@ function handleCellClick(event) {
     //判断黑白方
     if (playerType == 1) {
         //显示黑棋
-        event.target.classList.remove("cell-null")
-        event.target.classList.remove("cell-white")
-        event.target.classList.remove("cell-enter")
-        event.target.classList.add("cell-black")
+        // event.target.classList.remove("cell-null")
+        // event.target.classList.remove("cell-white")
+        // event.target.classList.remove("cell-enter")
+        // event.target.classList.add("cell-black")
 
 
         boardData[row][col] = 1
@@ -86,13 +94,16 @@ function handleCellClick(event) {
             }
         }
         chessManuals.push(chessManual)
+        if (checkWin(playerType, row, col)) {
+            console.log("Player:" + playerType + "Win!!!")
+        }
         playerType = 2
     } else if (playerType == 2) {
         //显示白棋
-        event.target.classList.remove("cell-null")
-        event.target.classList.remove("cell-black")
-        event.target.classList.remove("cell-enter")
-        event.target.classList.add("cell-white")
+        // event.target.classList.remove("cell-null")
+        // event.target.classList.remove("cell-black")
+        // event.target.classList.remove("cell-enter")
+        // event.target.classList.add("cell-white")
 
         boardData[row][col] = 2
         chessManual = {
@@ -102,9 +113,13 @@ function handleCellClick(event) {
             }
         }
         chessManuals.push(chessManual)
+        if (checkWin(playerType, row, col)) {
+            console.log("Player:" + playerType + "Win!!!")
+        }
         playerType = 1
 
     }
+    RenderBoard()
 
     console.log(boardData)
     console.log(chessManuals)
@@ -113,22 +128,95 @@ function handleCellClick(event) {
 function chooseBlock(event) {
     const row = parseInt(event.target.dataset.row);
     const col = parseInt(event.target.dataset.col);
+    if (boardData[row][col] == 0) {
+        //判断如果两次点击为同一个坐标这进行落子，
+        //否则清除上一次的样式，更新当前坐标样式
+        if (lastBlock.col == col && lastBlock.row == row) {
+            //落子
+            handleCellClick(event)
+        } else {
+            //删除上一次方格的样式
+            if (lastBlock.lastEvent != null) {
+                lastBlock.lastEvent.target.classList.remove("cell-enter")
+                // lastBlock.lastEvent.target.classList.add("cell-null")
+                console.log("ww")
+            }
 
-    if (lastBlock.col == col && lastBlock.row == row) {
-        handleCellClick(event)
-    } else {
-        if (lastBlock.lastEvent != null) {
-            lastBlock.lastEvent.target.classList.remove("cell-enter")
-            lastBlock.lastEvent.target.classList.add("cell-null")
-            console.log("ww")
+            event.target.classList.add("cell-enter")
+            lastBlock.col = col
+            lastBlock.row = row
+            lastBlock.lastEvent = event
         }
-
-        event.target.classList.add("cell-enter")
-        lastBlock.col = col
-        lastBlock.row = row
-        lastBlock.lastEvent = event
+        console.log(lastBlock)
+    } else {
+        console.log("您不能在已落子的地方落子")
     }
-    console.log(lastBlock)
+
+
+}
+//悔棋
+function RetractChess() {
+    boardData[chessManuals[chessManuals.length - 1].pos.y][chessManuals[chessManuals.length - 1].pos.x] = 0
+    chessManuals.pop()
+    boardData[chessManuals[chessManuals.length - 1].pos.y][chessManuals[chessManuals.length - 1].pos.x] = 0
+    chessManuals.pop()
+    RenderBoard()
 }
 
-initBoard()
+function checkWin(player, row, col) {
+    // 检查水平方向
+    let count = 0;
+    for (let i = col - 4; i <= col + 4; i++) {
+        if ( i >= 0 && i < 19&&boardData[row][i] === player ) {
+            count++;
+            if (count === 5) {
+                return true;
+            }
+        } else {
+            count = 0;
+        }
+    }
+
+    // 检查垂直方向
+    count = 0;
+    for (let i = row - 4; i <= row + 4; i++) {
+        if (i >= 0 && i < 19 &&boardData[i][col] === player ) {
+            count++;
+            if (count === 5) {
+                return true;
+            }
+        } else {
+            count = 0;
+        }
+    }
+
+    // 检查主对角线
+    count = 0;
+    for (let i = row - 4, j = col - 4; i <= row + 4 && j <= col + 4; i++, j++) {
+        if (i >= 0 && i < 19 && j >= 0 && j < 19 && boardData[i][j] === player) {
+            count++;
+            if (count === 5) {
+                return true;
+            }
+        } else {
+            count = 0;
+        }
+    }
+
+    // 检查副对角线
+    count = 0;
+    for (let i = row - 4, j = col + 4; i <= row + 4 && j >= col - 4; i++, j--) {
+        if (i >= 0 && i < 19 && j >= 0 && j < 19 && boardData[i][j] === player) {
+            count++;
+            if (count === 5) {
+                return true;
+            }
+        } else {
+            count = 0;
+        }
+    }
+
+
+    return false;
+}
+RenderBoard()
