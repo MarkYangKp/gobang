9b49affdfa3d8f6612a7e495af54e700e70d76ce
markyangkp <markyangkp@outlook.com>
2023-12-14 16:37:06 +0800

    qwdw

--- static/js/index.js
+++ static/js/index.js
@@ -3,11 +3,11 @@ const LocalUrl = "http://127.0.0.1:5000"
 const LocalServer1 = "http://10.1.1.99:5000"
 const LocalServer = LocalUrl
 document.addEventListener('DOMContentLoaded', function () {
-    const userName =  IsSetName()
-    if(userName){
+    const userName = IsSetName()
+    if (userName) {
         document.getElementById("userNameSpan").innerText = userName
-    }else{
-        document.getElementById("userNameSpan").innerText  = "无名小卒"
+    } else {
+        document.getElementById("userNameSpan").innerText = "无名小卒"
     }
     document.getElementById("SetNameButton").addEventListener("click", SetName)
     document.getElementById("SetName-exit").addEventListener("click", HiedSetNameBox)
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
 // const outils = require("./outils.min.js")
 var userID = undefined
 function modelCheck(e) {
-    if(IsSetName()){
+    if (IsSetName()) {
         console.log(e);
         var model = e.target.dataset.model;
         console.log(model);
@@ -40,10 +40,10 @@ function modelCheck(e) {
         } else if (model == "pve") {
             window.location.href = "gamePvE?userID=" + userID;
         }
-    }else{
+    } else {
         showSetNameBox()
     }
-    
+
 
     //qdqwdqd
 }
@@ -60,7 +60,7 @@ function SetUserID() {
 function IsSetName() {
     // 从localStorage获取用户name
     var userName = localStorage.getItem('user_name');
-    if (userName == undefined || userName == null || userName =="") {
+    if (userName == undefined || userName == null || userName == "") {
         return false
     } else {
         return userName
@@ -84,7 +84,7 @@ function SetName(e) {
         var userName = inputBox.value
         localStorage.setItem('user_name', userName);
         // 定义请求的URL
-        
+
         const url = LocalServer + '/setusername';
 
         // 构建要发送的数据
@@ -110,7 +110,7 @@ function SetName(e) {
             .then(data => {
                 // 处理响应数据
                 console.log(data);
-                if(data.code == 1){
+                if (data.code == 1) {
                     document.getElementById("userNameSpan").innerText = userName
                     HiedSetNameBox()
                     window.location.href = "/"
@@ -129,7 +129,7 @@ function GetRankData() {
     var userID = localStorage.getItem('user_id');
     var userName = localStorage.getItem('user_name');
     console.log(userName)
-    if (userName == null || userName== undefined) {
+    if (userName == null || userName == undefined) {
 
     } else {
         // 定义请求的URL
@@ -157,7 +157,12 @@ function GetRankData() {
             .then(data => {
                 // 处理响应数据
                 console.log(data);
-                CreatRankInfoBox(data.rank,data.userName,data.win,data.fail,data.peace,data.score)
+                if(data.code == -1){
+                    errorUserName()
+                }else{
+                    CreatRankInfoBox(data.rank, data.userName, data.win, data.fail, data.peace, data.score)
+                }
+                
                 // if (data.code == 1) {
                 //     document.getElementById("userNameSpan").innerText = userName
                 //     HiedSetNameBox()
@@ -189,19 +194,19 @@ function CreatRankInfoBox(rank, username, win, fail, peace, score) {
     rankBoxitem2.innerHTML = "<span>" + username + "</span>"
     var rankBoxitem3 = document.createElement("div")
     rankBoxitem3.classList.add("rankBox-item")
-    rankBoxitem3.innerHTML = "<span>"+win+"</span>/<span>"+fail+"</span>/<span>"+peace+"</span>"
+    rankBoxitem3.innerHTML = "<span>" + win + "</span>/<span>" + fail + "</span>/<span>" + peace + "</span>"
     var rankBoxitem4 = document.createElement("div")
     rankBoxitem4.classList.add("rankBox-item")
     rankBoxitem4.innerHTML = "<span>" + score + "</span>"
 
-    var rankBox = document.createElement("div") 
+    var rankBox = document.createElement("div")
     rankBox.classList.add("rankBox")
     rankBox.appendChild(rankBoxitem1)
     rankBox.appendChild(rankBoxitem2)
     rankBox.appendChild(rankBoxitem3)
     rankBox.appendChild(rankBoxitem4)
 
-    var userInfo =  document.createElement("div")
+    var userInfo = document.createElement("div")
     userInfo.classList.add("userInfo")
     userInfo.appendChild(rankBox)
     document.getElementById("rankBox").appendChild(userInfo)
@@ -214,5 +219,43 @@ function GetUserID() {
     }
 }
 
+function errorUserName() {
+    var userName = localStorage.getItem("user_name")
+    
+    const url = LocalServer + '/setusername';
 
+    // 构建要发送的数据
+    const data = {
+        userID: String(userID),
+        userName: userName
+    };
+
+    // 发起POST请求
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json' // 根据实际情况设置请求头
+        },
+        body: JSON.stringify(data) // 将数据转换为JSON字符串
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // 处理响应数据
+            console.log(data);
+            if (data.code == 1) {
+                // document.getElementById("userNameSpan").innerText = userName
+                // HiedSetNameBox()
+                window.location.href = "/"
+            }
+        })
+        .catch(error => {
+            // 处理错误
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+}
 GetUserID()
